dbf9cacb3a4843cd4712cf16f71887b7f61b2d1c
Witty04 <dwikumeilani@gmail.com>
2024-09-04 18:05:33 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,47 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Di Jepang ada eskrim cumi-cumi.",
+        answer: "Benar",
+        options: [
+            "Benar",
+            "Salah"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Burung Unta akan menaruh kepalanya di dalam tanah saat takut.",
+        answer: "Salah",
+        options: [
+            "Benar",
+            "Salah"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Sebenarnya di dunia ini ada pohon yang bisa berjalan.",
+        answer: "Benar",
+        options: [
+            "Benar",
+            "Salah"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Petir tidak pernah menyambar tempat yang sama dua kali.",
+        answer: "Salah",
+        options: [
+            "Benar",
+            "Salah"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Makan terlalu banyak permen dapat membuat diabetes",
+        answer: "Salah",
+        options: [
+            "Benar",
+            "Salah"
+        ]
+    },
+];
